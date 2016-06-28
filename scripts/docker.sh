a2b01435c5ee893c483b11f2b7b5d34e0166873a
ryane <ryanesc@gmail.com>
2016-06-28 19:13:14 -0400

    testing

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,6 +5,6 @@ set -ex
 docker pull hello-world
 docker tag hello-world ryane/hello-world-test:0.1
 docker images
-cat ./images
-<./images | xargs -r -n1 docker images --format "{{.Repository}}:{{.Tag}}"
-<./images | xargs -r -n1 docker images --format "{{.Repository}}:{{.Tag}}" | xargs -r -n1 docker push
+cat scripts/images
+<scripts/images xargs -r -n1 docker images --format "{{.Repository}}:{{.Tag}}"
+<scripts/images xargs -r -n1 docker images --format "{{.Repository}}:{{.Tag}}" | xargs -r -n1 docker push

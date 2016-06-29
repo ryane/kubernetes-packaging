553bef031fa2ed9fb75e03002726746ca20eea9b
ryane <ryanesc@gmail.com>
2016-06-28 21:32:04 -0400

    testing

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,12 +2,9 @@
 
 set -ex
 
-docker pull hello-world
-docker tag hello-world ryane/hello-world-test:0.1
-docker version
-docker images
 cat scripts/images
+docker images
 
 while read image; do
-  docker images | grep "$image" | awk '{printf "%s:%s\n", $1, $2}' | xargs -r -n1 docker push
+    docker images | grep "$image" | awk '{printf "%s:%s\n", $1, $2}' | xargs -r -n1 docker push
 done < scripts/images
